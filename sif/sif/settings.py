5e647f135605bd7c83b5eb26a2f6813236745ef7
Evgeny1337 <genya-sozykin@mal.ru>
2025-10-08 13:53:16 +0500

    init commit

--- sif/sif/settings.py
+++ sif/sif/settings.py
@@ -9,18 +9,20 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+import dj_database_url
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-irika^ktua#5@y0wj8!8=nlmfnp6r$1df!+0g3hv(u^35#f(cy'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -77,20 +79,12 @@ WSGI_APPLICATION = 'sif.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'default_db',
-        'USER': 'gen_user',
-        'PASSWORD': 'Sirenko2706200030',
-        'HOST': '194.87.84.236',
-        'PORT': '5432',
-        'OPTIONS': {
-            'options': '-c search_path=sif'
-        }
-    }
+    'default': env.db('DATABASE_URL')
 }
 
-
+DATABASES['default']['OPTIONS'] = {
+    'options': '-c search_path=sif'
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
