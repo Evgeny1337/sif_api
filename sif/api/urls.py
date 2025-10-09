4966aa16234751df708d3dd24044905b5b012e05
Evgeny1337 <genya-sozykin@mal.ru>
2025-10-09 14:40:51 +0500

    Добавил views и urls

--- sif/api/urls.py
+++ sif/api/urls.py
@@ -1,11 +1,20 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from .views import DictCatalogViewSet, DictSectionViewSet
+from .serializers import DictRoleSerializer
+from .views import DictCatalogViewSet, DictSectionViewSet, DictOrderStateViewSet, DictEmployeeViewSet, DictBookViewSet, \
+    DictUserViewSet, DictCatalogFileViewSet, OrderCatalogViewSet
 
 router = routers.DefaultRouter()
 router.register(r'catalog', DictCatalogViewSet)
 router.register(r'section',DictSectionViewSet)
+router.register(r'state', DictOrderStateViewSet)
+router.register(r'role', DictRoleSerializer)
+router.register(r'employee', DictEmployeeViewSet)
+router.register(r'book', DictBookViewSet)
+router.register(r'user',DictUserViewSet)
+router.register('file', DictCatalogFileViewSet)
+router.register(r'order',OrderCatalogViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
--- sif/api/views.py
+++ sif/api/views.py
@@ -1,7 +1,8 @@
 from rest_framework import permissions, viewsets
 
-from .models import DictSection, DictCatalog
-from .serializers import DictSectionSerializer, DictCatalogSerializer
+from .models import DictSection, DictCatalog, DictBook, DictUser, DictRole, DictEmployee, OrderCatalog, DictOrderState
+from .serializers import DictSectionSerializer, DictCatalogSerializer, DictBookSerializer, DictUserSerializer, \
+    DictRoleSerializer, DictEmployeeSerializer, DictCatalogFileSerializer, OrderStateSerializer
 
 
 class DictSectionViewSet(viewsets.ModelViewSet):
@@ -11,6 +12,36 @@ class DictSectionViewSet(viewsets.ModelViewSet):
 class DictCatalogViewSet(viewsets.ModelViewSet):
     queryset = DictCatalog.objects.all()
     serializer_class = DictCatalogSerializer
+
+class DictBookViewSet(viewsets.ModelViewSet):
+    queryset = DictBook.objects.all()
+    serializer_class = DictBookSerializer
+
+class DictUserViewSet(viewsets.ModelViewSet):
+    queryset = DictUser.objects.all()
+    serializer_class = DictUserSerializer
+
+class DictRoleViewSet(viewsets.ModelViewSet):
+    queryset = DictRole.objects.all()
+    serializer_class = DictRoleSerializer
+
+class DictEmployeeViewSet(viewsets.ModelViewSet):
+    queryset = DictEmployee.objects.all()
+    serializer_class = DictEmployeeSerializer
+
+class DictCatalogFileViewSet(viewsets.ModelViewSet):
+    queryset = DictCatalog.objects.all()
+    serializer_class = DictCatalogFileSerializer
+
+class OrderCatalogViewSet(viewsets.ModelViewSet):
+    queryset = OrderCatalog.objects.all()
+    serializer_class = DictCatalogFileSerializer
+
+class DictOrderStateViewSet(viewsets.ModelViewSet):
+    queryset = DictOrderState.objects.all()
+    serializer_class = OrderStateSerializer
+
+
 
 
 
